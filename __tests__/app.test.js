d1e2544b5b9d30670f8bff2011e4b41eae96eb1d
HannahHan9 <h.han9204@gmail.com>
2023-06-27 15:26:24 +0100

    get articles by ID

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -9,7 +9,7 @@ beforeEach(() => seed(testData));
 afterAll(() => db.end());
 
 describe("GET /api/topics", () => {
-  test("200:should respond with the correct length of the array", () => {
+  test("200:should respond with the correct length of the topics array", () => {
     return request(app)
       .get("/api/topics")
       .expect(200)
@@ -40,13 +40,46 @@ describe("GET /api", () => {
       });
   });
 });
+describe("GET /api/articles/:article_id", () => {
+  test("200:should respond with an individual article", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.article).toHaveProperty("article_id", expect.any(Number));
+        expect(body.article).toHaveProperty("title", expect.any(String));
+        expect(body.article).toHaveProperty("topic", expect.any(String));
+        expect(body.article).toHaveProperty("author", expect.any(String));
+        expect(body.article).toHaveProperty("body", expect.any(String));
+        expect(body.article).toHaveProperty("created_at", expect.any(String));
+        expect(body.article).toHaveProperty("votes", expect.any(Number));
+        expect(body.article).toHaveProperty("article_img_url", expect.any(String));
+      });
+  });
+  test("400:should respond with bad request for an invalid article id", () => {
+    return request(app)
+      .get("/api/articles/NotAnId")
+      .expect(400)
+      .then(({ body }) => {
+        expect(body.msg).toBe("Bad request");
+      });
+  });
+  test("404:should respond with Not Found when passed an id that does not exist", () => {
+    return request(app)
+      .get("/api/articles/9999")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toBe("ID Not Found")
+      });
+  });
+});
 describe("All non-existent path", () => {
   test("404: should return a custom error message when the path is not found", () => {
     return request(app)
       .get("/api/notapath")
       .expect(404)
       .then(({ body }) => {
-        expect(body.msg).toBe("Not Found");
+        expect(body.msg).toBe("Route Not Found");
       });
   });
 });
--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 const express = require("express");
-const { getApiTopics, getApis } = require("./controllers/app.controllers");
-const { handleServerErrors } = require("./errors");
+const {
+  getApiTopics,
+  getApis,
+  getArticleById,
+} = require("./controllers/app.controllers");
+const {
+  handlePsqlErrors,
+  handleCustomErrors,
+  handleServerErrors,
+} = require("./errors");
 const app = express();
-
 
 app.get("/api/topics", getApiTopics);
 
 app.get("/api", getApis);
 
+app.get("/api/articles/:article_id", getArticleById);
+
 app.all("*", (_, res) => {
-  res.status(404).send({ status: 404, msg: "Not Found" });
+  res.status(404).send({ status: 404, msg: "Route Not Found" });
 });
+
+app.use(handlePsqlErrors);
+
+app.use(handleCustomErrors);
 
 app.use(handleServerErrors);
 
