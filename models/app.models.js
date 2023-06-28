a7a30c39388661f7666f00b543aa26713de7ad41
HannahHan9 <h.han9204@gmail.com>
2023-06-28 01:18:13 +0100

    get all articles sorted by date in descending order w/o console.log

--- models/app.models.js
+++ models/app.models.js
@@ -21,7 +21,6 @@ exports.selectAllArticles = () => {
       `SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.article_id) AS comment_count FROM articles FULL JOIN comments ON articles.article_id = comments.article_id GROUP BY articles.article_id ORDER BY articles.created_at DESC;`
     )
     .then(({ rows }) => {
-      console.log(rows);
       if (!rows.length) {
         return Promise.reject({ status: 404, msg: "Not Found" });
       } else return rows;
