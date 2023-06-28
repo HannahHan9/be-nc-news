c0206f3688f6c3bb00afead59997f087f46e43da
HannahHan9 <h.han9204@gmail.com>
2023-06-28 01:15:07 +0100

    get all articles sorted by date in descending order

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -13,8 +13,8 @@ describe("GET /api/topics", () => {
     return request(app)
       .get("/api/topics")
       .expect(200)
-      .then((res) => {
-        expect(res.body.topics.length).toBe(3);
+      .then(({ body }) => {
+        expect(body.topics.length).toBe(3);
       });
   });
   test("200:should respond with all topics", () => {
@@ -73,6 +73,45 @@ describe("GET /api/articles/:article_id", () => {
       .expect(404)
       .then(({ body }) => {
         expect(body.msg).toBe("ID Not Found");
+      });
+  });
+});
+describe("GET /api/articles", () => {
+  test("200:should respond with the correct length of the articles array", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.articles.length).toBe(13);
+      });
+  });
+  test("200:should respond with all articles", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body }) => {
+        expect(Array.isArray(body.articles)).toBe(true);
+        body.articles.forEach((article) => {
+          expect(article).toMatchObject({
+            author: expect.any(String),
+            title: expect.any(String),
+            article_id: expect.any(Number),
+            topic: expect.any(String),
+            created_at: expect.any(String),
+            votes: expect.any(Number),
+            article_img_url: expect.any(String),
+            comment_count: expect.any(String),
+          });
+        });
+      });
+  });
+  test("200: articles should be sorted by date in descending order", () => {
+    return request(app)
+      .get("/api/articles")
+      .expect(200)
+      .then(({ body }) => {
+        const { articles } = body;
+        expect(articles).toBeSortedBy("created_at", { descending: true });
       });
   });
 });
--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const {
   getApiTopics,
   getApis,
   getArticleById,
+  getAllArticles,
 } = require("./controllers/app.controllers");
 const {
   handlePsqlErrors,
@@ -16,6 +17,8 @@ app.get("/api/topics", getApiTopics);
 app.get("/api", getApis);
 
 app.get("/api/articles/:article_id", getArticleById);
+
+app.get("/api/articles", getAllArticles);
 
 app.all("*", (_, res) => {
   res.status(404).send({ status: 404, msg: "Route Not Found" });
